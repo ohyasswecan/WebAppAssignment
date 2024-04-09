dbeebdef54b9b86f62b15b7dd392f58c1f4dce57
Yasswecancn <63643014+ohyasswecan@users.noreply.github.com>
2024-04-10 11:40:49 +1200

    Class Detail page has been designed in this version. However, it lacks of flexibility. Requires better understanding in genericview, and listview

--- gradebook/urls.py
+++ gradebook/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.home_views, name='home'),
     path('CourseList/', views.course_list_views, name='courselist'),
     path('ClassList/', views.class_list_views, name='classlist'),
+    path('ClassList/ClassDetail/<int:class_id>/', views.generic_details_views, name='classdetail'),
     path('SemesterList/', views.semester_list_views, name='semesterlist'),
     path('StudentList/', views.student_list_views, name='studentlist'),
     path('LecturerList/', views.lecturer_list_views, name='lecturerlist'),
