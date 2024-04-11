17d58a33ac8264765c34c60743c72482308531da
Yasswecancn <63643014+ohyasswecan@users.noreply.github.com>
2024-04-11 12:58:55 +1200

    Generic view implemented for all view functions. DetailView and ListView has been implied to all sections.

--- gradebook/urls.py
+++ gradebook/urls.py
@@ -2,15 +2,22 @@ from django.contrib.auth import admin
 from django.urls import path
 
 from gradebook import views
-from gradebook.views import ClassEnrollmentListView, ClassEnrollmentDetailView
+from gradebook.views import ClassEnrollmentListView, ClassEnrollmentDetailView, CourseListView, CourseDetailView, \
+    EnrollmentDetailView, EnrollmentListView, LecturerListView, LecturerDetailView, SemesterListView, \
+    SemesterDetailView, StudentListView, StudentDetailView
 
 urlpatterns = [
     path('', views.home_views, name='home'),
-    path('CourseList/', views.course_list_views, name='courselist'),
+    path('CourseList/', CourseListView.as_view(), name='courselist'),
+    path('CourseList/CourseDetail/<int:pk>/', CourseDetailView.as_view(), name='coursedetail'),
     path('ClassList/', ClassEnrollmentListView.as_view(), name='classlist'),
     path('ClassList/ClassDetail/<int:pk>/', ClassEnrollmentDetailView.as_view(), name='classdetail'),
-    path('SemesterList/', views.semester_list_views, name='semesterlist'),
-    path('StudentList/', views.student_list_views, name='studentlist'),
-    path('LecturerList/', views.lecturer_list_views, name='lecturerlist'),
-    path('EnrollmentList/', views.enrollment_list_views, name='enrollmentlist'),
+    path('SemesterList/', SemesterListView.as_view(), name='semesterlist'),
+    path('SemesterList/SemesterDetail/<int:pk>', SemesterDetailView.as_view(), name='semesterdetail'),
+    path('StudentList/', StudentListView.as_view(), name='studentlist'),
+    path('StudentList/StudentDetail/<int:pk>', StudentDetailView.as_view(), name='studentdetail'),
+    path('LecturerList/', LecturerListView.as_view(), name='lecturerlist'),
+    path('LecturerList/LecturerDetail/<int:pk>/', LecturerDetailView.as_view(), name='lecturerdetail'),
+    path('EnrollmentList/', EnrollmentListView.as_view(), name='enrollmentlist'),
+    path('EnrollmentList/EnrollmentDetail/<int:student_id>/<int:class_id>/', EnrollmentDetailView.as_view(), name='enrollmentdetail')
 ]
--- gradebook/views.py
+++ gradebook/views.py
@@ -45,47 +45,140 @@ class ClassEnrollmentDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
-def course_list_views(request):
-    course_list = Course.objects.all()
-    template = loader.get_template('CourseList.html')
-    context = {
-        'course_list': course_list
-    }
-    return HttpResponse(template.render(context, request))
+#Template view version course list view
+# def course_list_views(request):
+#     course_list = Course.objects.all()
+#     template = loader.get_template('CourseList.html')
+#     context = {
+#         'course_list': course_list
+#     }
+#     return HttpResponse(template.render(context, request))
 
-def semester_list_views(request):
-    semester_list = Semester.objects.all()
-    template = loader.get_template('SemesterList.html')
-    context = {
-        'semester_list':semester_list
-    }
-    return HttpResponse(template.render(context,request))
+class CourseListView(ListView):
+    model = Course
+    template_name = 'CourseList.html'
+    ordering = ['course_id']
 
-def student_list_views(request):
-    student_list = Student.objects.all()
-    template = loader.get_template('StudentList.html')
-    context = {
-        'student_list':student_list
-    }
-    return HttpResponse(template.render(context, request))
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
-def lecturer_list_views(request):
-    lecturer_list = Lecturer.objects.all()
-    template = loader.get_template('LecturerList.html')
-    context = {
-        'lecturer_list':lecturer_list
-    }
-    return HttpResponse(template.render(context, request))
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'CourseDetailPage.html'
 
-def enrollment_list_views(request):
-    enrollment_list = Student_Enrollment.objects.all()
-    template = loader.get_template('EnrollmentList.html')
-    context = {
-        'enrollment_list':enrollment_list
-    }
-    return HttpResponse(template.render(context, request))
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+#Tempalte version semester list view
+# def semester_list_views(request):
+#     semester_list = Semester.objects.all()
+#     template = loader.get_template('SemesterList.html')
+#     context = {
+#         'semester_list':semester_list
+#     }
+#     return HttpResponse(template.render(context,request))
+
+class SemesterListView(ListView):
+    model = Semester
+    template_name = 'SemesterList.html'
+    ordering = ['semester_id']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class SemesterDetailView(DetailView):
+    model = Semester
+    template_name = 'SemesterDetailPage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+#Template Version student list view
+# def student_list_views(request):
+#     student_list = Student.objects.all()
+#     template = loader.get_template('StudentList.html')
+#     context = {
+#         'student_list':student_list
+#     }
+#     return HttpResponse(template.render(context, request))
+
+class StudentListView(ListView):
+    model = Student
+    template_name = 'StudentList.html'
+    ordering = ['student_id']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = 'StudentDetailPage.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+#Template version lecturer list view
+# def lecturer_list_views(request):
+#     lecturer_list = Lecturer.objects.all()
+#     template = loader.get_template('LecturerList.html')
+#     context = {
+#         'lecturer_list':lecturer_list
+#     }
+#     return HttpResponse(template.render(context, request))
+
+class LecturerListView(ListView):
+    model = Lecturer
+    template_name = 'LecturerList.html'
+    ordering = ['staff_id']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class LecturerDetailView(DetailView):
+    model = Lecturer
+    template_name = 'LecturerDetailPage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+#Template version student enrollment list view
+# def enrollment_list_views(request):
+#     enrollment_list = Student_Enrollment.objects.all()
+#     template = loader.get_template('EnrollmentList.html')
+#     context = {
+#         'enrollment_list':enrollment_list
+#     }
+#     return HttpResponse(template.render(context, request))
+
+class EnrollmentListView(ListView):
+    model = Student_Enrollment
+    template_name = 'EnrollmentList.html'
+    ordering = ['-enrollment_date'] #most recent enrolled student to most early enrolled student
 
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class EnrollmentDetailView(DetailView):
+    model = Student_Enrollment
+    template_name = 'EnrollmentDetailPage.html'
+
+    def get_object(self, queryset=None):
+        student_id = self.kwargs.get('student_id')
+        class_id = self.kwargs.get('class_id')
+        queryset = self.get_queryset() #query filling  #queryset().filter(field1 = value1) to search by field value
+        return queryset.get(student_id=student_id, class_id=class_id)#composite key query
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 
 
